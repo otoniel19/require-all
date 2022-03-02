86145a2992aece6b6909c9319bc26fbd8cd93812
otoniel19 <otonielcostaoliveira3@gmail.com>
2022-03-02 16:24:40 -0300

    1.0.2

--- index.js
+++ index.js
@@ -3,9 +3,12 @@
  * @param {String} module the module to import all
  */
 module.exports = (module) => {
+  try {
+    require(module);
+  } catch (e) {
+    throw new Error(e.message);
+  }
   var m = require(module);
-  if (typeof m != "object")
-    throw new Error(`module is not an object of exports`);
 
   Object.keys(m).forEach((key) => (global[key] = m[key]));
 };
